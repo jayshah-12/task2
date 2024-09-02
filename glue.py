0f7e0641598226cb4dd9ac03a4d9eed357735598
Jay Shah <89582065+jayshah-12@users.noreply.github.com>
2024-09-02 15:48:46 +0530

    Update glue.py

--- glue.py
+++ glue.py
@@ -1,23 +1,17 @@
 import os
 
-def get_env_var(var_name):
-    """Read the environment variable."""
-    value = os.getenv(var_name)
-    if value is None:
-        raise ValueError(f"Environment variable {var_name} not found.")
-    return value
+
 
 def convert_name_to_uppercase(name):
     """Convert the name to uppercase."""
-    return name.upper()
+    
+    return name.swapcase()
 
 if __name__ == "__main__":
     # Load environment variables from the .env file
-    from dotenv import load_dotenv
-    load_dotenv(dotenv_path='secrets-output/name.env')
     
     # Fetch the name from the environment
-    name = get_env_var('NAME')
+    name = os.getenv('NAME')
     
     # Convert the name to uppercase
     uppercase_name = convert_name_to_uppercase(name)
